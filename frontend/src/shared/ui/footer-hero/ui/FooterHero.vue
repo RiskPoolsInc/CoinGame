2c5c29204025647f5ba9a7798f237825a42c3277
Khursandov <xursandov_s@mail.ru>
2023-11-20 15:02:04 +0500

    RP-7 added footer hero added

--- frontend/src/shared/ui/footer-hero/ui/FooterHero.vue
+++ frontend/src/shared/ui/footer-hero/ui/FooterHero.vue
@@ -3,8 +3,31 @@
 <template>
   <div class="footer-hero">
     <div class="row container justify-between">
-      <div class="column-lg-5">2</div>
-      <div class="column-lg-5">2</div>
+      <div class="col-lg-5">
+        <div class="row footer-hero__list">
+          <div class="col-lg-1 my-auto">
+            <q-icon name="telegram" size="sm" />
+          </div>
+          <div class="col-lg-11 footer-hero__item">Telegram chat</div>
+
+          <div class="col-lg-1 my-auto">
+            <q-icon name="mail" size="sm" />
+          </div>
+          <div class="col-lg-11 footer-hero__item">
+            Tech support: support_coin@riskpools.io
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-5">
+        <div class="row footer-hero__list">
+          <div class="col-lg-1 my-auto">
+            <q-icon name="people" size="sm" />
+          </div>
+          <div class="col-lg-11 footer-hero__item">
+            Tech support: support_coin@riskpools.io
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
