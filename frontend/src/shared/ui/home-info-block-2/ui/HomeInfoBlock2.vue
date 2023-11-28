c5d751da97bd5055df2855059169d11284156dc6
Khursandov <xursandov_s@mail.ru>
2023-11-28 13:11:03 +0500

    home page info block chart fixed

--- frontend/src/shared/ui/home-info-block-2/ui/HomeInfoBlock2.vue
+++ frontend/src/shared/ui/home-info-block-2/ui/HomeInfoBlock2.vue
@@ -15,15 +15,15 @@ defineProps<IInfoBlockProps>();
       <div class="home-info-block-2__title">
         {{ data.title }}
       </div>
-      <div class="home-info-block-2__body row">
-        <div class="home-info-block-2__content col-lg-7">
+      <div class="home-info-block-2__body row justify-between">
+        <div class="home-info-block-2__content col-lg-7 col-md-7">
           <p>
             In this classic children’s game, one player tosses a coin and covers
             it with their hand – while the other has to guess if it’s heads or
             tails. In our scenario, the user plays against a risk pool. Guess
             right – and you’ll get the reward from the pool; guess wrong – and
-            the reward will go to the pool holder. But all is not as simple as
-            it seems…
+            the reward will go to the pool holder. <br />
+            But all is not as simple as it seems…
           </p>
         </div>
         <img
@@ -32,11 +32,12 @@ defineProps<IInfoBlockProps>();
           alt="info-block-2"
         />
         <VButton
-          class="home-info-block-2__btn"
+          class="col-lg-3 col-md-4 home-info-block-2__btn"
           size="lg"
           label="FULL EXAMPLE"
           color="white"
           text-color="dark"
+          padding="9px 59px"
           to="/example"
         />
       </div>
