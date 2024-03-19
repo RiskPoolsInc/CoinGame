93dd26b6f2f2fa7a556235fea66c7a01760da3a7
Boris Timofeenko <boriswinner88@gmail.com>
2024-03-19 19:00:16 +0300

    Cron perform refund in backend

--- backend/app.js
+++ backend/app.js
@@ -6,55 +6,56 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./openapi.json');
 const port = 3000
+var cron = require('node-cron');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('This is RiskPools backend. Docs: /api-docs')
 })
 
 app.get('/upload-game-wallet', async (req, res) => {
     let address = req.query.address;
     let privateKey = req.query.privateKey;
     let publicKey = req.query.publicKey;
-    let gameWallets = await storage.getItem("gameWallets");
-    gameWallets.push({
+    let gameWallet = {
         address: address,
         privateKey: privateKey,
         publicKey: publicKey,
-    });
-    await storage.setItem("gameWallets", gameWallets);
-    console.log('Wallet')
-    console.log(await storage.getItem("gameWallets"))
-    performRefunds();
-    return { code: 200 };
+    };
+    await storage.setItem("gameWallet_" + address, gameWallet);
+
+    // performRefunds();
+    cron.schedule('* * 24 * *', () => {
+        performRefund(address);
+    })
+    return res.status(200).json({ success: true })
 })
 
-async function performRefunds() {
-    console.log('Refunds:')
-    let gameWallets = await storage.getItem("gameWallets");
-    for (let i = 0; i < gameWallets.length; i++) {
-        let gameWalletCilUtils = new CilUtils({
-            privateKey: gameWallets[i].privateKey,
-            apiUrl: 'https://test-explorer.ubikiri.com/api/',
-            rpcPort: 443,
-            rpcAddress: 'https://rpc-dv-1.ubikiri.com/',
-            rpcUser: 'cilTest',
-            rpcPass: 'd49c1d2735536baa4de1cc6'
-        });
-        await gameWalletCilUtils.asyncLoaded();   
-        const txList = await gameWalletCilUtils.getTXList();
-        for (j = 0; j < txList.length; j++) {
-            if (txList[j].outputs.length == 1 && txList[j].outputs[0].to == gameWallets[i].address) {
-                console.log('Performing refund');
-                const arrUtxos = await gameWalletCilUtils.getUtxos();
-                const txCost = gameWalletCilUtils._estimateTxFee(arrUtxos.length, 1, true);
-                const sumToSend = txList[j].outputs[0].amount - txCost;
-                const txFunds = await gameWalletCilUtils.createSendCoinsTx([
-                    [txList[j].inputs[0].from, sumToSend]
-                  ], 0);
-                await gameWalletCilUtils.waitTxDoneExplorer(txFunds.getHash());
-            }
+async function performRefund(walletAddress) {
+    console.log('Performing refunds for wallet: ' + walletAddress);
+    let gameWallet = await storage.getItem("gameWallet_" + walletAddress);
+    let gameWalletCilUtils = new CilUtils({
+        privateKey: gameWallet.privateKey,
+        apiUrl: 'https://test-explorer.ubikiri.com/api/',
+        rpcPort: 443,
+        rpcAddress: 'https://rpc-dv-1.ubikiri.com/',
+        rpcUser: 'cilTest',
+        rpcPass: 'd49c1d2735536baa4de1cc6'
+    });
+    await gameWalletCilUtils.asyncLoaded();   
+    const txList = await gameWalletCilUtils.getTXList();
+    for (j = 0; j < txList.length; j++) {
+        if (txList[j].outputs.length == 1 && txList[j].outputs[0].to == gameWallet.address) {
+            console.log('Performing refund');
+            console.log(txList[j]);
+            const arrUtxos = await gameWalletCilUtils.getUtxos();
+            const txCost = gameWalletCilUtils._estimateTxFee(arrUtxos.length, 1, true);
+            const sumToSend = txList[j].outputs[0].amount - txCost;
+            const txFunds = await gameWalletCilUtils.createSendCoinsTx([
+                [txList[j].inputs[0].from, sumToSend]
+                ], 0);
+            await gameWalletCilUtils.waitTxDoneExplorer(txFunds.getHash());
             console.log('Refunded ' + sumToSend + ' UBX to: ' + txList[j].inputs[0].from)
         }
     }
@@ -62,19 +63,5 @@ async function performRefunds() {
 
 app.listen(port, async () => {
     await storage.init( /* options ... */ );
-    let gameWallets = await storage.getItem('gameWallets');
-    if (!gameWallets) {
-        await storage.setItem("gameWallets", []);
-    }
-    // let t = crypto.createKeyPair();
-    // console.log(t.address);
-    // console.log(t.privateKey);
-    // console.log(t.publicKey)
-    let gameWalletCilUtils = new CilUtils({
-        privateKey: "c833d9950b328984d9484b6314c659108bf125ee30a2d4c888e933f52bb3bcef",
-        apiUrl: 'https://test-explorer.ubikiri.com/api/',
-        rpcPort: 443,
-        rpcAddress: 'https://rpc-dv-1.ubikiri.com/',
-        rpcUser: 'cilTest',
     console.log(`Example app listening on port ${port}`);
 })
