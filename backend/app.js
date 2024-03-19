7a698d69fd2e243e00553a0e3533d65ee867b5df
Boris Timofeenko <boriswinner88@gmail.com>
2024-03-19 09:32:30 +0300

    Initial draft of backend

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+const { get, set } = require("node-global-storage");
+const crypto = require('crypto-web');
+const CilUtils = require('cil-utils');
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./openapi.json');
+const port = 3000
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/upload-game-wallet', async (req, res) => {
+    let address = req.query.address;
+    let privateKey = req.query.privateKey;
+    let publicKey = req.query.publicKey;
+    let gameWallets = get("gameWallets");
+    gameWallets.push({
+        address: address,
+        privateKey: privateKey,
+        publicKey: publicKey,
+    });
+    set("gameWallets", gameWallets);
+    console.log(get("gameWallets"))
+    // gameWalletCilUtils = new CilUtils({
+    //     privateKey: privateKey,
+    //     apiUrl: 'https://test-explorer.ubikiri.com/api/',
+    //     rpcPort: 443,
+    //     rpcAddress: 'https://rpc-dv-1.ubikiri.com/',
+    //     rpcUser: 'cilTest',
+    //     rpcPass: 'd49c1d2735536baa4de1cc6'
+    //   });
+    //   await gameWalletCilUtils.asyncLoaded();
+    return { code: 200 };
+})
+
+app.listen(port, () => {
+    set("gameWallets", []);
+    console.log(`Example app listening on port ${port}`);
+})
