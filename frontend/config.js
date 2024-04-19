eb8c6fbc5b0001292a2b06211ac40e0da66e9302
CBEPK <stas.avramchuk@gmail.com>
2024-04-19 13:39:55 +0300

    [CI SKIP]

--- frontend/config.js
+++ frontend/config.js
@@ -3,7 +3,7 @@ const CIL_UTILS_RPC_PORT = 443
 const CIL_UTILS_RPC_ADDRESS = "https://rpc-dv-1.ubikiri.com/"
 const CIL_UTILS_RPC_USER = "cilTest"
 const CIL_UTILS_RPC_PASS = "d49c1d2735536baa4de1cc6"
-const BACKEND_URL = "http://localhost:3000"
+const BACKEND_URL = "https://api.coingame.dev20021.ubikiri.com/"
 
 export {
     CIL_UTILS_API_URL,
