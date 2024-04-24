183afa3c949db164ad931523ddb643e16958179b
Boris Timofeenko <boriswinner88@gmail.com>
2024-04-24 11:52:40 +0300

    Add bid validation on frontend

--- frontend/src/features/game/bid-card/ui/BidCard.vue
+++ frontend/src/features/game/bid-card/ui/BidCard.vue
@@ -1,92 +1,75 @@
 <script setup lang="ts">
-import VInput from "@/shared/ui/base-components/v-input/ui/VInput.vue";
-import VButton from "@/shared/ui/base-components/v-button/ui/VButton.vue";
-import { useGameStore } from "@/entities/game/model/game";
-import { computed, ref, watch } from "vue";
+  import VInput from "@/shared/ui/base-components/v-input/ui/VInput.vue";
+  import VButton from "@/shared/ui/base-components/v-button/ui/VButton.vue";
+  import { useGameStore } from "@/entities/game/model/game";
+  import { computed, ref, watch } from "vue";
+  import { MIN_BID, MAX_BID } from '../../../../../config.js'
 
-const { gameState, startGame } = useGameStore();
+  const { gameState, startGame } = useGameStore();
 
-const confirm = ref(false);
-const error = ref(false);
-let slider = ref(3);
+  const confirm = ref(false);
+  const error = ref(false);
+  let slider = ref(3);
 
-watch(slider, (newVal) => {
-  if (newVal < 3) {
-    slider.value = 3;
-    return;
-  }
-  gameState.round = newVal;
-});
+  watch(slider, (newVal) => {
+    if (newVal < 3) {
+      slider.value = 3;
+      return;
+    }
+    gameState.round = newVal;
+  });
 
-const statusPlayButton = computed(() => {
-  return !(
-    Number(gameState.bid) >= 10000 &&
-    gameState.round >= 3 &&
-    gameState.wallet &&
-    !gameState.inProgress
-  );
-});
+  const statusPlayButton = computed(() => {
+    return !(
+      Number(gameState.bid) >= MIN_BID &&
+      Number(gameState.bid) <= MAX_BID &&
+      gameState.round >= 3 &&
+      gameState.round <= 10 &&
+      gameState.wallet &&
+      !gameState.inProgress
+    );
+  });
 
-const onStart = () => {
-  if (Number(gameState.bid) > gameState.balance) {
-    error.value = true;
-    return;
-  }
+  const onStart = () => {
+    if (Number(gameState.bid) > gameState.balance) {
+      error.value = true;
+      return;
+    }
 
-  confirm.value = true;
-};
+    confirm.value = true;
+  };
 </script>
 
 <template>
   <div class="bid-card">
     <div class="row justify-center">
       <div class="col-lg-4 col-md-4 col-sm-5 col-xs-10">
-        <VInput
-          class="bid-card__input"
-          v-model="gameState.bid"
-          label="Your bid, UBX"
-        />
+        <VInput class="bid-card__input" v-model="gameState.bid" label="Your bid, UBX"
+          :rules="[val => (Number(val) >= MIN_BID && Number(val) <= MAX_BID) || 'Bid should be between ' + MIN_BID + ' and ' + MAX_BID + ' UBX']" />
       </div>
     </div>
 
     <div class="bid-card__slider">
       <div class="bid-card__label">Number of rounds (3-10)</div>
 
-      <q-slider
-        v-model="slider"
-        color="white"
-        marker-labels
-        :min="3"
-        :max="10"
-      />
+      <q-slider v-model="slider" color="white" marker-labels :min="3" :max="10" />
     </div>
 
     <div class="bid-card__action row justify-center">
       <div class="col-lg-3 col-md-3 col-sm-3 col-xs-8 bid-card__btn">
-        <VButton
-          @click="onStart"
-          :disabled="!!statusPlayButton"
-          className="full-width"
-          label="TOSS A COIN"
-          color="white"
-          size="lg"
-          text-color="dark"
-        />
+        <VButton @click="onStart" :disabled="!!statusPlayButton" className="full-width" label="TOSS A COIN"
+          color="white" size="lg" text-color="dark" />
       </div>
     </div>
 
     <q-dialog v-model="confirm" persistent>
-      <q-card
-        dark
-        style="
+      <q-card dark style="
           width: 491px;
           background: #50596c;
           border-radius: 7px;
           border: 1px solid #bfc9e2;
           box-shadow: none;
-        "
-        class="bid-card__dialog"
-      >
+        " class="bid-card__dialog">
         <q-card-section class="row items-center justify-center">
           <span class="bid-card__popup-text q-ml-sm">
             The bet is placed. Start the game?
@@ -95,45 +78,27 @@ const onStart = () => {
 
         <q-card-actions align="center" class="bid-card__popup-actions">
           <div class="col-lg-3 col-xs-4">
-            <VButton
-              class-name="full-width"
-              label="Play"
-              color="white"
-              text-color="dark"
-              size="lg"
-              @click="startGame"
-              v-close-popup
-            />
+            <VButton class-name="full-width" label="Play" color="white" text-color="dark" size="lg" @click="startGame"
+              v-close-popup />
           </div>
 
           <div class="col-lg-3 col-xs-4">
-            <VButton
-              class-name="full-width"
-              label="Cancel"
-              color="white"
-              text-color="dark"
-              size="lg"
-              @click="confirm = false"
-              v-close-popup
-            />
+            <VButton class-name="full-width" label="Cancel" color="white" text-color="dark" size="lg"
+              @click="confirm = false" v-close-popup />
           </div>
         </q-card-actions>
       </q-card>
     </q-dialog>
 
     <q-dialog v-model="error" persistent>
-      <q-card
-        dark
-        style="
+      <q-card dark style="
           width: 100%;
           max-width: 491px;
           background: #50596c;
           border-radius: 7px;
           border: 1px solid #bfc9e2;
           box-shadow: none;
-        "
-        class="bid-card__dialog"
-      >
+        " class="bid-card__dialog">
         <q-card-section class="row items-center justify-center p-0">
           <div class="col-12 bid-card__popup-text bid-card__popup-text--error">
             <span class="bid-card__popup-text bid-card__popup-text--error">
@@ -149,14 +114,7 @@ const onStart = () => {
 
         <q-card-actions align="center" class="bid-card__popup-actions">
           <div class="col-lg-3 col-xs-4">
-            <VButton
-              class-name="full-width"
-              label="OK"
-              color="white"
-              text-color="dark"
-              size="lg"
-              v-close-popup
-            />
+            <VButton class-name="full-width" label="OK" color="white" text-color="dark" size="lg" v-close-popup />
           </div>
         </q-card-actions>
       </q-card>
@@ -165,31 +123,33 @@ const onStart = () => {
 </template>
 
 <style scoped lang="scss">
-@import "./styles.module";
+  @import "./styles.module";
 </style>
 <style lang="scss">
-.bid-card__input {
-  .q-field--dark .q-field__control:before {
-    border: 3px solid #bfc9e2;
-  }
-}
-.q-slider__marker-labels {
-  font-family: Padauk;
-  font-size: 16px;
-  color: #fff;
-}
-.bid-card {
-  .v-input__label {
-    //position: absolute;
-    //margin-left: -10%;
-  }
-}
-
-@media (max-width: 550px) {
-  .bid-card {
-    .v-input__label {
-      margin-left: -10%;
+  .bid-card__input {
+    .q-field--dark .q-field__control:before {
+      border: 3px solid #bfc9e2;
     }
   }
-}
+
+  .q-slider__marker-labels {
+    font-family: Padauk;
+    font-size: 16px;
+    color: #fff;
+  }
+
+  .bid-card {
+    .v-input__label {
+      //position: absolute;
+      //margin-left: -10%;
+    }
+  }
+
+  @media (max-width: 550px) {
+    .bid-card {
+      .v-input__label {
+        margin-left: -10%;
+      }
+    }
+  }
 </style>
--- frontend/src/shared/ui/base-components/v-input/ui/VInput.vue
+++ frontend/src/shared/ui/base-components/v-input/ui/VInput.vue
@@ -1,57 +1,48 @@
 <script setup lang="ts">
-import { computed, defineEmits, defineProps, withDefaults } from "vue";
+  import { computed, defineEmits, defineProps, withDefaults } from "vue";
 
-type InputValueType = string | number | undefined;
-interface IVButtonProps {
-  label?: string;
-  modelValue: InputValueType;
-  topLabel?: boolean;
-  leftLabel?: boolean;
-  disabled?: boolean;
-}
-const props = withDefaults(defineProps<IVButtonProps>(), {
-  label: undefined,
-  topLabel: undefined,
-});
+  type InputValueType = string | number | undefined;
+  interface IVButtonProps {
+    label?: string;
+    modelValue: InputValueType;
+    topLabel?: boolean;
+    leftLabel?: boolean;
+    disabled?: boolean;
+    rules?: any,
+  }
+  const props = withDefaults(defineProps<IVButtonProps>(), {
+    label: undefined,
+    topLabel: undefined,
+  });
 
-const emit = defineEmits<{
-  "update:modelValue": [InputValueType];
-}>();
+  const emit = defineEmits<{
+    "update:modelValue": [InputValueType];
+  }>();
 
-const inputValue = computed({
-  get: () => props.modelValue,
-  set: (val) => emit("update:modelValue", val),
-});
+  const inputValue = computed({
+    get: () => props.modelValue,
+    set: (val) => emit("update:modelValue", val),
+  });
 </script>
 
 <template>
-  <div
-    class="v-input"
-    :class="[
-      {
-        'top-label': topLabel,
-        'left-label': leftLabel,
-      },
-    ]"
-  >
+  <div class="v-input" :class="[
+    {
+      'top-label': topLabel,
+      'left-label': leftLabel,
+    },
+  ]">
     <label v-if="label" class="v-input__label"> {{ label }} </label>
-    <q-input
-      class="v-input__input"
-      outlined
-      :disable="disabled"
-      v-model="inputValue"
-      autocomplete="off"
-      dark
-      lazy-rules
-    />
+    <q-input class="v-input__input" outlined :disable="disabled" v-model="inputValue" autocomplete="off" dark lazy-rules
+      :rules=rules />
   </div>
 </template>
 
 <style scoped lang="scss">
-@import "./styles.module";
+  @import "./styles.module";
 </style>
 <style lang="scss">
-.q-field--outlined .q-field__control {
-  border-radius: 7px;
-}
+  .q-field--outlined .q-field__control {
+    border-radius: 7px;
+  }
 </style>
