533310ceca3807bb8b9bf9e351b4aede158ff9de
Boris Timofeenko <boriswinner88@gmail.com>
2024-05-02 12:01:09 +0300

    Remove unused variables. Update readme.

--- backend/helpers.js
+++ backend/helpers.js
@@ -22,8 +22,8 @@ function hash2Number(hash) {
 const initCilUtils = async () => {
 
     global.poolWalletKeyPair = { "address": process.env.POOL_WALLET_ADDRESS, "privateKey": process.env.POOL_WALLET_PRIVATE_KEY, "publicKey": process.env.POOL_WALLET_PUBLIC_KEY }
-    global.projectWalletKeyPair = { "address": process.env.PROJECT_WALLET_ADDRESS, "privateKey": process.env.PROJECT_WALLET_PRIVATE_KEY, "publicKey": process.env.PROJECT_WALLET_PUBLIC_KEY }
-    global.profitWalletKeyPair = { "address": process.env.PROFIT_WALLET_ADDRESS, "privateKey": process.env.PROFIT_WALLET_PRIVATE_KEY, "publicKey": process.env.PROFIT_WALLET_PUBLIC_KEY }
+    global.projectWalletKeyPair = { "address": process.env.PROJECT_WALLET_ADDRESS }
+    global.profitWalletKeyPair = { "address": process.env.PROFIT_WALLET_ADDRESS }
 
     global.poolWalletCilUtils = new CilUtils({
         privateKey: global.poolWalletKeyPair.privateKey,
@@ -34,26 +34,6 @@ const initCilUtils = async () => {
         rpcPass: process.env.CIL_UTILS_RPC_PASS
     });
     await global.poolWalletCilUtils.asyncLoaded();
-
-    global.projectWalletCilUtils = new CilUtils({
-        privateKey: global.projectWalletKeyPair.privateKey,
-        apiUrl: process.env.CIL_UTILS_API_URL,
-        rpcPort: process.env.CIL_UTILS_RPC_PORT,
-        rpcAddress: process.env.CIL_UTILS_RPC_ADDRESS,
-        rpcUser: process.env.CIL_UTILS_RPC_USER,
-        rpcPass: process.env.CIL_UTILS_RPC_PASS
-    });
-    await global.projectWalletCilUtils.asyncLoaded();
-
-    global.profitWalletCilUtils = new CilUtils({
-        privateKey: global.profitWalletKeyPair.privateKey,
-        apiUrl: process.env.CIL_UTILS_API_URL,
-        rpcPort: process.env.CIL_UTILS_RPC_PORT,
-        rpcAddress: process.env.CIL_UTILS_RPC_ADDRESS,
-        rpcUser: process.env.CIL_UTILS_RPC_USER,
-        rpcPass: process.env.CIL_UTILS_RPC_PASS
-    });
-    await global.profitWalletCilUtils.asyncLoaded();
 }
 
 module.exports = {
