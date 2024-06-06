ae8ba49b2daa4104e07e225414da66316625f5c0
Boris Timofeenko <boriswinner88@gmail.com>
2024-06-06 21:27:00 +0300

    Add right padding to grid

--- frontend/src/entities/game/ui/balance-chart/ui/BalanceChart.vue
+++ frontend/src/entities/game/ui/balance-chart/ui/BalanceChart.vue
@@ -1,152 +1,155 @@
 <script setup lang="ts">
-import { computed, onMounted, ref, watch } from "vue";
-import ApexCharts from "apexcharts";
-import { useGameStore } from "@/entities/game/model/game";
+  import { computed, onMounted, ref, watch } from "vue";
+  import ApexCharts from "apexcharts";
+  import { useGameStore } from "@/entities/game/model/game";
 
-const { gameState } = useGameStore();
+  const { gameState } = useGameStore();
 
-const list = ref<number[]>([]);
-const chart = ref<ApexCharts | null>(null);
-const interval = ref<number | null>(null);
-const datasets = computed(() => {
-  const parityList = gameState.parityList.map(item => item.currentBalance);
-  return parityList.length > 0 ? [gameState.bidForBalanceChart, ...parityList] : [];
-});
+  const list = ref<number[]>([]);
+  const chart = ref<ApexCharts | null>(null);
+  const interval = ref<number | null>(null);
+  const datasets = computed(() => {
+    const parityList = gameState.parityList.map(item => item.currentBalance);
+    return parityList.length > 0 ? [gameState.bidForBalanceChart, ...parityList] : [];
+  });
 
-///const annotations = computed(() => points.value.map(item => item.y))
+  ///const annotations = computed(() => points.value.map(item => item.y))
 
-const points = computed(() => {
-  return gameState.parityList.map(item => makePoint(item.currentBalance, item.round));
-});
+  const points = computed(() => {
+    return gameState.parityList.map(item => makePoint(item.currentBalance, item.round));
+  });
 
-watch(() => gameState.inProgress, (newValue) => {
-  if (newValue) {
-    list.value = []
-    updateChart()
-  }
-})
+  watch(() => gameState.inProgress, (newValue) => {
+    if (newValue) {
+      list.value = []
+      updateChart()
+    }
+  })
 
-watch(datasets, (newSet) => {
-  if (interval.value) {
-    clearInterval(interval.value);
-  }
-
-  let i = list.value.length;
-  interval.value = setInterval(() => {
-    if (i === newSet.length || !newSet.length) {
-      clearInterval(interval.value!);
-      return;
+  watch(datasets, (newSet) => {
+    if (interval.value) {
+      clearInterval(interval.value);
     }
 
-    add2List(newSet[i]);
+    let i = list.value.length;
+    interval.value = setInterval(() => {
+      if (i === newSet.length || !newSet.length) {
+        clearInterval(interval.value!);
+        return;
+      }
 
-    updateChart();
+      add2List(newSet[i]);
 
-    i++;
-  }, 1000);
-});
+      updateChart();
 
-const makePoint = (value: number, round: number) => ({
-  x: round + 1,
-  y: value,
-  marker: {
-    size: 8.5,
-    fillColor: "rgba(145, 150, 219, 0.50)",
-    strokeColor: "#9196DB",
-  },
-  label: {
-    offsetY: 0,
-    borderColor: "transparent",
-    style: {
-      color: "#BFC9E2",
-      background: "transparent",
-      fontFamily: "Padauk",
-      fontSize: "12px",
-      fontWeight: 700,
-    },
-    text: value.toString(),
-  },
-});
-
-const add2List = (value: number) => {
-  list.value = [...list.value, value];
-};
-
-const updateChart = () => {
-  chart.value?.updateSeries([{ data: list.value }]);
-  chart.value?.updateOptions({
-    annotations: { points: points.value },
+      i++;
+    }, 1000);
   });
-};
 
-onMounted(() => {
-  const options = {
-    series: [{ name: "Round", data: list.value }],
-    labels: Array.from({ length: 11 }, (_, i) => i),
-    annotations: { points: points.value },
-    chart: {
-      background: "#18212E",
-      height: 350,
-      type: "line",
-      dropShadow: {
-        enabled: true,
-        color: "blue",
-        top: 18,
-        left: 7,
-        blur: 10,
-        opacity: 0.2,
+  const makePoint = (value: number, round: number) => ({
+    x: round + 1,
+    y: value,
+    marker: {
+      size: 8.5,
+      fillColor: "rgba(145, 150, 219, 0.50)",
+      strokeColor: "#9196DB",
+    },
+    label: {
+      offsetY: 0,
+      borderColor: "transparent",
+      style: {
+        color: "#BFC9E2",
+        background: "transparent",
+        fontFamily: "Padauk",
+        fontSize: "12px",
+        fontWeight: 700,
       },
-      toolbar: { show: false, tools: { zoom: false } },
-      zoom: { enabled: false },
-      fontFamily: "Padauk",
-      offsetY: -20,
+      text: value.toString(),
     },
-    tooltip: { enabled: false },
-    colors: ["#9196DB", "#9196DB"],
-    dataLabels: { enabled: false },
-    stroke: { curve: "straight" },
-    grid: {
-      show: true,
-      borderColor: "rgba(191, 201, 226, 0.50)",
-      strokeDashArray: 0,
-      position: "back",
-      xaxis: { lines: { show: true } },
-      yaxis: { lines: { show: true } },
-    },
-    markers: { size: 0 },
-    xaxis: {
-      type: "numeric",
-      tickAmount: 10,
-      max: 10,
-      labels: {
-        style: {
-          colors: "#BFC9E2",
-          fontSize: "16px",
-          fontFamily: "Padauk",
-          fontWeight: 700,
-        },
-        formatter: (val: string) => parseInt(val),
-      },
-    },
-    yaxis: {
-      show: true,
-      showForNullSeries: false,
-      labels: {
-        style: {
-          colors: "#BFC9E2",
-          fontSize: "16px",
-          fontFamily: "Padauk",
-          fontWeight: 700,
-        },
-      },
-      showAlways: false,
-    },
-    legend: { display: false },
+  });
+
+  const add2List = (value: number) => {
+    list.value = [...list.value, value];
   };
 
-  chart.value = new ApexCharts(document.querySelector("#chart") as HTMLElement, options);
-  chart.value.render();
-});
+  const updateChart = () => {
+    chart.value?.updateSeries([{ data: list.value }]);
+    chart.value?.updateOptions({
+      annotations: { points: points.value },
+    });
+  };
+
+  onMounted(() => {
+    const options = {
+      series: [{ name: "Round", data: list.value }],
+      labels: Array.from({ length: 11 }, (_, i) => i),
+      annotations: { points: points.value },
+      chart: {
+        background: "#18212E",
+        height: 350,
+        type: "line",
+        dropShadow: {
+          enabled: true,
+          color: "blue",
+          top: 18,
+          left: 7,
+          blur: 10,
+          opacity: 0.2,
+        },
+        toolbar: { show: false, tools: { zoom: false } },
+        zoom: { enabled: false },
+        fontFamily: "Padauk",
+        offsetY: -20,
+      },
+      tooltip: { enabled: false },
+      colors: ["#9196DB", "#9196DB"],
+      dataLabels: { enabled: false },
+      stroke: { curve: "straight" },
+      grid: {
+        show: true,
+        borderColor: "rgba(191, 201, 226, 0.50)",
+        strokeDashArray: 0,
+        position: "back",
+        xaxis: { lines: { show: true } },
+        yaxis: { lines: { show: true } },
+        padding: {
+          right: 20
+        }
+      },
+      markers: { size: 0 },
+      xaxis: {
+        type: "numeric",
+        tickAmount: 10,
+        max: 10,
+        labels: {
+          style: {
+            colors: "#BFC9E2",
+            fontSize: "16px",
+            fontFamily: "Padauk",
+            fontWeight: 700,
+          },
+          formatter: (val: string) => parseInt(val),
+        },
+      },
+      yaxis: {
+        show: true,
+        showForNullSeries: false,
+        labels: {
+          style: {
+            colors: "#BFC9E2",
+            fontSize: "16px",
+            fontFamily: "Padauk",
+            fontWeight: 700,
+          },
+        },
+        showAlways: false,
+      },
+      legend: { display: false },
+    };
+
+    chart.value = new ApexCharts(document.querySelector("#chart") as HTMLElement, options);
+    chart.value.render();
+  });
 </script>
 
 <template>
@@ -157,23 +160,23 @@ onMounted(() => {
 </template>
 
 <style scoped lang="scss">
-#acquisitions {
-  margin-left: -10px;
-  margin-top: 20px;
-}
-
-.title {
-  margin: -20px auto 10px auto;
-  color: var(--color-white);
-  font-size: 22px;
-  font-weight: 700;
-  text-align: center;
-  font-family: Padauk;
-}
-
-@media (max-width: 550px) {
-  .title {
-    font-size: 14px;
+  #acquisitions {
+    margin-left: -10px;
+    margin-top: 20px;
   }
-}
+
+  .title {
+    margin: -20px auto 10px auto;
+    color: var(--color-white);
+    font-size: 22px;
+    font-weight: 700;
+    text-align: center;
+    font-family: Padauk;
+  }
+
+  @media (max-width: 550px) {
+    .title {
+      font-size: 14px;
+    }
+  }
 </style>
