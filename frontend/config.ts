7b34e3d37685447e1ea484f2a80aa125203be78d
CBEPK <stas.avramchuk@gmail.com>
2024-06-07 18:06:23 +0300

    release

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,9 +1,9 @@
-const CIL_UTILS_API_URL = "https://test-explorer.ubikiri.com/api/"
+const CIL_UTILS_API_URL = "https://explorer.ubikiri.com/api/"
 const CIL_UTILS_RPC_PORT = 443
-const CIL_UTILS_RPC_ADDRESS = "https://rpc-dv-1.ubikiri.com/"
-const CIL_UTILS_RPC_USER = "cilTest"
-const CIL_UTILS_RPC_PASS = "d49c1d2735536baa4de1cc6"
-const BACKEND_URL = process.env.NODE_ENV === 'local' ? "http://localhost:3000/" : "https://api.coingame.dev20021.ubikiri.com/"
+const CIL_UTILS_RPC_ADDRESS = "https://rpc-pr-1.ubikiri.com/"
+const CIL_UTILS_RPC_USER = "ubikiri"
+const CIL_UTILS_RPC_PASS = "622ca88c4e2ea80217"
+const BACKEND_URL = "https://api.riskpool.io/"
 const MIN_BID = 10000
 const MAX_BID = 10000000
 
--- frontend/docker/99-url-replacement.sh
+++ frontend/docker/99-url-replacement.sh
@@ -9,10 +9,14 @@ if [ ! -z $DEVENV ] && [ $DEVENV = "dev" ]; then
 fi
 
 
-if [ ! -z $DEVENV ] && [ $DEVENV = "prodio" ]; then
-    echo  "replace ENV to prod 4token io = $@"
-    sed -i 's@fortoken.ubikiri.com@4tokens.io@g' /usr/share/nginx/html/*.js
-    sed -i 's@fortoken-api.ubikiri.com@api.4tokens.io@g' /usr/share/nginx/html/*.js
+if [ ! -z $DEVENV ] && [ $DEVENV = "prod" ]; then
+    echo  "replace ENV to prod = $@"
+    sed -i 's@cilTest@ubikiri@g' /usr/share/nginx/html/*.js
+    sed -i 's@d49c1d2735536baa4de1cc6@622ca88c4e2ea80217@g' /usr/share/nginx/html/*.js
+    sed -i 's@api.coingame.dev20021.ubikiri.com@api.riskpool.io@g' /usr/share/nginx/html/*.js
+    sed -i 's@test-explorer.ubikiri.com@explorer.ubikiri.com@g' /usr/share/nginx/html/*.js
+    sed -i 's@rpc-dv-1@rpc-pr-1@g' /usr/share/nginx/html/*.js
+    sed -i 's@dev20021.ubikiri.com@ubikiri.com@g' /usr/share/nginx/html/*.js
 fi
 
 if [ ! -z $DEVENV ] && [ $DEVENV = "stage" ]; then
