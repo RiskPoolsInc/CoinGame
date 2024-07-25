06be1688504606433ad02ac803a9073643b23728
artyomtikhanov <artyomtikhanov@gmail.com>
2024-07-25 20:14:41 +0300

    fix TransactionRewardView.cs WalletHashFrom property name mapping

--- api/CG.Api/Base/App.Core.ViewModels/Transactions/TransactionRewardView.cs
+++ api/CG.Api/Base/App.Core.ViewModels/Transactions/TransactionRewardView.cs
@@ -3,7 +3,7 @@ using App.Core.ViewModels.Dictionaries;
 namespace App.Core.ViewModels.Transactions;
 
 public class TransactionRewardView : BaseView {
-    public string WalletFrom { get; set; }
+    public string WalletHashFrom { get; set; }
     public string TransactionHash { get; set; }
     public decimal Sum { get; set; }
     public decimal Fee { get; set; }
